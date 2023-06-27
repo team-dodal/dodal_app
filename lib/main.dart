c59cedc7a393245a4b489e013efe3dd1a211ace5
seolhs04 <shs00421@gmail.com>
2023-06-27 17:47:10 +0900

    마이페이지 달력 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:dodal_app/screens/main_route/main.dart';
 import 'package:dodal_app/screens/sign_in/main.dart';
+import 'package:dodal_app/theme/theme_data.dart';
 import 'package:dodal_app/utilities/fcm.dart';
 import 'package:flutter/material.dart';
-import 'package:dodal_app/theme/theme_data.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'firebase_options.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +16,7 @@ void main() async {
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Fcm.init();
+  await initializeDateFormatting();
 
   runApp(const App());
 }
@@ -35,6 +37,8 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: '도달',
       theme: lightTheme,
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      themeMode: ThemeMode.dark,
       home: FutureBuilder(
         future: _isTokenExist(),
         builder: (ctx, snapshot) {
--- lib/screens/main_route/mypage_screen.dart
+++ lib/screens/main_route/mypage_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dodal_app/screens/modify_user/main.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class MyPageScreen extends StatelessWidget {
   const MyPageScreen({super.key});
@@ -10,13 +11,35 @@ class MyPageScreen extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('MyPage'),
           ElevatedButton(
             onPressed: () {
               Navigator.of(context).push(MaterialPageRoute(
                   builder: (ctx) => const ModifyUserScreen()));
             },
             child: const Text('유저 수정'),
+          ),
+          TableCalendar(
+            firstDay: DateTime.utc(2023, 1, 1),
+            lastDay: DateTime.utc(2033, 3, 14),
+            focusedDay: DateTime.now(),
+            locale: 'ko-KR',
+            headerStyle: const HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+            ),
+            calendarStyle: CalendarStyle(
+              weekendTextStyle: const TextStyle(color: Colors.red),
+              markerSizeScale: 1,
+              markersAnchor: 1,
+              markerDecoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.4),
+                shape: BoxShape.circle,
+              ),
+            ),
+            eventLoader: (day) {
+              if (day.day % 2 == 0) return ['test'];
+              return [];
+            },
           )
         ],
       ),
