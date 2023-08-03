bf21d33c705607b60c8b8abceb87ec21316abfea
seolhs04 <shs00421@gmail.com>
2023-08-03 21:17:49 +0900

    카테고리 리스트 조회 구현 (수정중)

--- lib/screens/challenge_list/main.dart
+++ lib/screens/challenge_list/main.dart
@@ -1,11 +1,12 @@
+import 'package:dodal_app/providers/challenge_list_filter_cubit.dart';
+import 'package:dodal_app/services/challenge_service.dart';
 import 'package:dodal_app/widgets/challenge_list/tab_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class ChallengeListScreen extends StatefulWidget {
-  const ChallengeListScreen({
-    super.key,
-  });
+  const ChallengeListScreen({super.key});
 
   @override
   State<ChallengeListScreen> createState() => _ChallengeListScreenState();
@@ -14,16 +15,34 @@ class ChallengeListScreen extends StatefulWidget {
 class _ChallengeListScreenState extends State<ChallengeListScreen>
     with TickerProviderStateMixin {
   static const pageSize = 20;
-  final PagingController<int, dynamic> pagingController =
+  final PagingController<int, Challenge> pagingController =
       PagingController(firstPageKey: 0);
 
-  _request(int pageKey) async {}
+  _request(int pageKey) async {
+    final state = BlocProvider.of<ChallengeListFilterCubit>(context).state;
+    List<Challenge>? res = await ChallengeService.getChallengesByCategory(
+      categoryValue: state.category.value,
+      tagValue: state.tag.value,
+      conditionCode: state.conditionCode,
+      certCntList: state.certCntList,
+      page: pageKey,
+    );
+    if (res == null) return;
+    final isLastPage = res.length < pageSize;
+    if (isLastPage) {
+      pagingController.appendLastPage(res);
+    } else {
+      final nextPageKey = pageKey + res.length;
+      pagingController.appendPage(res, nextPageKey);
+    }
+  }
 
   @override
   void initState() {
     pagingController.addPageRequestListener((pageKey) {
       _request(pageKey);
     });
+
     super.initState();
   }
 
@@ -37,14 +56,14 @@ class _ChallengeListScreenState extends State<ChallengeListScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(bottom: const ListTabBar()),
-      // body: PagedListView<int, dynamic>(
-      //   pagingController: pagingController,
-      //   builderDelegate: PagedChildBuilderDelegate<dynamic>(
-      //     itemBuilder: (context, item, index) {
-      //       return const Text('data');
-      //     },
-      //   ),
-      // ),
+      body: PagedListView<int, Challenge>(
+        pagingController: pagingController,
+        builderDelegate: PagedChildBuilderDelegate<Challenge>(
+          itemBuilder: (context, item, index) {
+            return Text(item.title);
+          },
+        ),
+      ),
     );
   }
 }
--- lib/services/challenge_service.dart
+++ lib/services/challenge_service.dart
@@ -1,7 +1,45 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:dodal_app/model/tag_model.dart';
 import 'package:dodal_app/services/common/error_dialog.dart';
 import 'common/main.dart';
+
+class Challenge {
+  final int id;
+  final int adminId;
+  final String adminNickname;
+  final String? adminProfile;
+  final String title;
+  final int certCnt;
+  final String? thumbnailImg;
+  final int recruitCnt;
+  final int userCnt;
+  final int bookmarkCnt;
+  final bool bookmarkStatus;
+  final DateTime? registeredAt;
+  final String categoryName;
+  final String categoryValue;
+  final Tag tag;
+
+  Challenge.fromJson(Map<String, dynamic> data)
+      : id = data['challenge_room_id'],
+        adminId = data['user_id'],
+        adminNickname = data['nickname'],
+        adminProfile = data['profile_url'],
+        title = data['title'],
+        certCnt = data['cert_cnt'],
+        thumbnailImg = data['thumbnail_img_url'],
+        recruitCnt = data['recruit_cnt'],
+        userCnt = data['user_cnt'],
+        bookmarkCnt = data['bookmark_cnt'],
+        bookmarkStatus = data['bookmark_yn'] == 'Y' ? true : false,
+        registeredAt = data['registered_at'] != null
+            ? DateTime.parse(data['registered_at'])
+            : null,
+        categoryName = data['category_name'],
+        categoryValue = data['category_value'],
+        tag = Tag(name: data['tag_name'], value: data['tag_value']);
+}
 
 class ChallengeService {
   static createChallenge({
@@ -44,7 +82,7 @@ class ChallengeService {
     }
   }
 
-  static getChallengesByCategory({
+  static Future<List<Challenge>?> getChallengesByCategory({
     required String categoryValue,
     required String tagValue,
     required int conditionCode,
@@ -60,9 +98,11 @@ class ChallengeService {
         requestUrl += 'cert_cnt_list=$certCnt&';
       }
       requestUrl += 'page=$page&page_size=20';
-      print(requestUrl);
       final res = await service.get(requestUrl);
-      return res.data['result'];
+      List<dynamic> contents = res.data['result']['content'];
+      List<Challenge> result =
+          contents.map((item) => Challenge.fromJson(item)).toList();
+      return result;
     } on DioException catch (error) {
       ResponseErrorDialog(error);
       return null;
