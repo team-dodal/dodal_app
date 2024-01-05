51db236a7eb1bc027205123410bf94def4f2243f
seolhs04 <shs00421@gmail.com>
2024-01-05 14:51:22 +0900

    makefile -> shell file 수정

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ bot_name="앱 배포 알림봇"
 msg1="도달의 새로운 버전이 배포되었습니다"
 msg2="배포되는데 시간이 조금 걸릴수 있어 10분정도 뒤에 테스트해보세요!"
 
-
 create_keystore () {
     rm -rf "$keystore_path/key.properties"
     touch "$keystore_path/key.properties"
@@ -26,7 +25,7 @@ deploy_android () {
     cd ..
     source $(pwd)/deploy.history
     curl -H "Content-Type: application/json" \
-         -d "{\"username\": \"$bot_name\", \"content\": \"**$msg1**\n> 대상: 안드로이드\n> 버전: $get_last_version\n> 빌드 넘버: $ANDROID_BUILD_NUMBER\n> $play_store_address\n$msg2\"}" \
+         -d "{\"username\": \"$bot_name\", \"content\": \"**$msg1**\n> 대상: 안드로이드\n> 버전: $get_last_version\n> 빌드 넘버: $ANDROID_BUILD_NUMBER\n> $PLAY_STORE_ADDRESS\n$msg2\"}" \
          "$DISCORD_URL"
     rm $(pwd)/deploy.history
 }
@@ -36,12 +35,22 @@ deploy_ios () {
     cd $(pwd)/ios
     fastlane beta version:$get_last_version password:$KEY_PASSWORD
     cd ..
-    source $(pwd)/deploy.history
     curl -H "Content-Type: application/json" \
-         -d "{\"username\": \"$bot_name\", \"content\": \"**$msg1**\n> 대상: IOS\n> 버전: $get_last_version\n> 빌드 넘버: $IOS_BUILD_NUMBER\n$msg2\"}" \
+         -d "{\"username\": \"$bot_name\", \"content\": \"**$msg1**\n> 대상: IOS\n> 버전: $get_last_version\n> 빌드 넘버: $build_number\n$msg2\"}" \
          "$DISCORD_URL"
     rm $(pwd)/deploy.history
 }
 
-# deploy_android
-deploy_ios
+PLATFORM=$1
+case "$PLATFORM" in
+    "android")
+    deploy_android
+    ;;
+    "ios")
+    deploy_ios
+    ;;
+    "start")
+    deploy_android
+    deploy_ios
+    ;;
+esac
